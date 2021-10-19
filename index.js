548648288aa17599e259b9ff7a60f79c07a5f467
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-19 13:43:30 +0100

    Deleting an Error Log

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const cron = require('node-cron');
 const express = require('express');
-
+const fs = require('fs')
 
 
 app = express();
 
 // Schedule tasks to be run on the server.
-cron.schedule('* * * * *', function() {
-  console.log('running a task every minute');
+//cron.schedule('* * * * *', function() {
+ // console.log('running a task every minute');
+//});
+
+
+// Remove the error.log file every twenty-first day of the month.
+cron.schedule('0 0 21 * *', function() {
+  console.log('---------------------');
+  console.log('Running Cron Job');
+  fs.unlink('./error.log', err => {
+    if (err) throw err;
+    console.log('Error file successfully deleted');
+  });
 });
 
 
