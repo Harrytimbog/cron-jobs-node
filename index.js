7b341a8d35deee66b2dedc6108d862ef002c9bf8
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-19 14:30:23 +0100

    Exploring Using node-cron to Send Scheduled Emails

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const express = require('express');
 const fs = require('fs')
-
+const nodemailer = require('nodemailer');
 
 app = express();
 
@@ -18,6 +18,36 @@ cron.schedule('0 0 21 * *', function() {
   fs.unlink('./error.log', err => {
     if (err) throw err;
     console.log('Error file successfully deleted');
+  });
+});
+
+// Sending emails every Wednesday.
+cron.schedule('0 0 * * 3', function () {
+  console.log('---------------------');
+  console.log('Running Cron Job');
+
+  let transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "5e0ad524876d4b",
+      pass: "978a471096c37a"
+    }
+  });
+
+  const message = {
+    from: 'Harriemannie@gmail.com', // Sender address
+    to: 'Harrytimbog@gmail.com',         // List of recipients
+    subject: 'Beulah is My Girlfriend', // Subject line
+    html: '<h1>I plan to marry her even though she get!</h1><p> <b>Coconut head</b> !</p>'
+    // text: 'Purchase your Holystone drones! Get your Holystone' // Plain text body
+  };
+  transport.sendMail(message, function (err, info) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(info);
+    }
   });
 });
 
